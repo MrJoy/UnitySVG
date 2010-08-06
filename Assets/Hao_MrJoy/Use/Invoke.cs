655eeffc924734c6ec5690e6e65bd236512bd5ba
Jon Frisby <jfrisby@mrjoy.com>
2010-08-06 15:35:04 -0500

    Add some big-picture timers...

--- Assets/Hao_MrJoy/Use/Invoke.cs
+++ Assets/Hao_MrJoy/Use/Invoke.cs
@@ -1,4 +1,6 @@
 using UnityEngine;
+using System;
+using System.Diagnostics;
 
 public class Invoke : MonoBehaviour {
 	public TextAsset SVGFile = null;
@@ -6,13 +8,35 @@ public class Invoke : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
 		if (SVGFile != null) {
+		  Stopwatch w = new Stopwatch();
+		  
+			w.Reset();
+			w.Start();
 			m_implement = new Implement(this.SVGFile);
+			w.Stop();
+			long c = w.ElapsedMilliseconds;
+			
+			w.Reset();
+			w.Start();
 			m_implement.f_StartProcess();
+			w.Stop();
+			long p = w.ElapsedMilliseconds;
+			
+			w.Reset();
+			w.Start();
 			renderer.material.mainTexture = m_implement.f_GetTexture();	
+			w.Stop();
+			long r = w.ElapsedMilliseconds;
+			UnityEngine.Debug.Log("Construction: " + Format(c) + ", Processing: " + Format(p) + ", Rendering: " + Format(r));
+			
 			Vector2 ts = renderer.material.mainTextureScale;
 			ts.x *= -1;
 			renderer.material.mainTextureScale = ts;
 			renderer.material.mainTexture.filterMode = FilterMode.Trilinear;
 		}
+	}
+	
+	private string Format(long ts) {
+	  return String.Format("{0} ms", ts);
 	}
 }
