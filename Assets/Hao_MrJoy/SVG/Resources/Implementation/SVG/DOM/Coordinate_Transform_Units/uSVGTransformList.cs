86ace3e6997e54a5a42172ae9c331c1d0305864e
Jon Frisby <jfrisby@mrjoy.com>
2010-08-06 21:34:50 -0500

    Cache matrix to avoid needless recalculation.

--- Assets/Hao_MrJoy/SVG/Resources/Implementation/SVG/DOM/Coordinate_Transform_Units/uSVGTransformList.cs
+++ Assets/Hao_MrJoy/SVG/Resources/Implementation/SVG/DOM/Coordinate_Transform_Units/uSVGTransformList.cs
@@ -6,17 +6,21 @@ public class uSVGTransformList {
 	public int Count {
 		get{ return m_listTransform.Count; }
 	}
+	private uSVGMatrix _totalMatrix = null;
 	public uSVGMatrix totalMatrix {
 		get {
-			if (m_listTransform.Count == 0) {
-				return new uSVGMatrix(1.0f, 0.0f, 0.0f, 1.0f, 0.0f, 0.0f);
-			} else {
-				uSVGMatrix matrix = m_listTransform[0].matrix;
-				for (int i = 1; i < m_listTransform.Count; i++) {
-					matrix = matrix.Multiply(m_listTransform[i].matrix);
-				}
-				return matrix;
+		  if(_totalMatrix == null) {
+  			if (m_listTransform.Count == 0) {
+  				_totalMatrix = new uSVGMatrix(1.0f, 0.0f, 0.0f, 1.0f, 0.0f, 0.0f);
+  			} else {
+  				uSVGMatrix matrix = m_listTransform[0].matrix;
+  				for (int i = 1; i < m_listTransform.Count; i++) {
+  					matrix = matrix.Multiply(m_listTransform[i].matrix);
+  				}
+  				_totalMatrix = matrix;
+  			}
 			}
+			return _totalMatrix;
 		}
 	}
 	/*********************************************************************************************/
@@ -29,13 +33,15 @@ public class uSVGTransformList {
 	public uSVGTransformList(string listString) {
 		m_listTransform = uSVGStringExtractor.f_ExtractTransformList(listString);
 	}
-	
+
 	/*********************************************************************************************/
 	public void AppendItem(uSVGTransform newItem) {
 		m_listTransform.Add(newItem);
+	  _totalMatrix = null;
 	}
 	public void AppendItems(uSVGTransformList newListItem) {
 	  m_listTransform.AddRange(newListItem.m_listTransform);
+	  _totalMatrix = null;
 	}
 	public uSVGTransform this[int index] {
 	  get {
