405135909e2b7007df5679662d3808e370a71fe3
Jon Frisby <jfrisby@mrjoy.com>
2010-08-06 20:19:32 -0500

    Style cleanup.

--- Assets/Hao_MrJoy/SVG/Resources/Implementation/SVG/DOM/BasicTypes/uSVGLength.cs
+++ Assets/Hao_MrJoy/SVG/Resources/Implementation/SVG/DOM/BasicTypes/uSVGLength.cs
@@ -1,76 +1,75 @@
 public enum uSVGLengthType : ushort {
-	SVG_LENGTHTYPE_UNKNOWN = 0,
-	SVG_LENGTHTYPE_NUMBER = 1,
-	SVG_LENGTHTYPE_PERCENTAGE = 2,
-	SVG_LENGTHTYPE_EMS = 3,
-	SVG_LENGTHTYPE_EXS = 4,
-	SVG_LENGTHTYPE_PX = 5,
-	SVG_LENGTHTYPE_CM = 6,
-	SVG_LENGTHTYPE_MM = 7,
-	SVG_LENGTHTYPE_IN = 8,
-	SVG_LENGTHTYPE_PT = 9,
-	SVG_LENGTHTYPE_PC = 10,
+  SVG_LENGTHTYPE_UNKNOWN = 0,
+  SVG_LENGTHTYPE_NUMBER = 1,
+  SVG_LENGTHTYPE_PERCENTAGE = 2,
+  SVG_LENGTHTYPE_EMS = 3,
+  SVG_LENGTHTYPE_EXS = 4,
+  SVG_LENGTHTYPE_PX = 5,
+  SVG_LENGTHTYPE_CM = 6,
+  SVG_LENGTHTYPE_MM = 7,
+  SVG_LENGTHTYPE_IN = 8,
+  SVG_LENGTHTYPE_PT = 9,
+  SVG_LENGTHTYPE_PC = 10,
 }
 /**************************************************************************************************/
 public struct uSVGLength  {
-		private uSVGLengthType m_unitType;
-		private float m_valueInSpecifiedUnits;
+  private uSVGLengthType m_unitType;
+  private float m_valueInSpecifiedUnits;
 
-		/***********************************************************************************/
-		public float value {
-			get{return uSVGLengthConvertor.f_ConvertToPX(m_valueInSpecifiedUnits, m_unitType);}
-		}
-		public uSVGLengthType unitType {
-			get{ return m_unitType;}
-		}
-		/***********************************************************************************/
-		public uSVGLength(ushort unitType, float valueInSpecifiedUnits) {
-			m_unitType = (uSVGLengthType)unitType;
-			m_valueInSpecifiedUnits = valueInSpecifiedUnits;
-		}
-		public uSVGLength(float valueInSpecifiedUnits) {
-			m_unitType = (uSVGLengthType)0;
-			m_valueInSpecifiedUnits = valueInSpecifiedUnits;
-		}
-		public uSVGLength(string valueText) {
-			float t_value = 0.0f;
-			uSVGLengthType t_type = uSVGLengthType.SVG_LENGTHTYPE_UNKNOWN;
-			uSVGLengthConvertor.f_ExtractType(valueText, ref t_value, ref t_type);
-			m_unitType = t_type;			
-			m_valueInSpecifiedUnits = t_value;
-		}
-		/***********************************************************************************/
-		public void NewValueSpecifiedUnits(ushort unitType, float valueInSpecifiedUnits) {
-			m_unitType = (uSVGLengthType)unitType;
-			m_valueInSpecifiedUnits = valueInSpecifiedUnits;
-		}
-		public void NewValueSpecifiedUnits(float valueInSpecifiedUnits) {
-			NewValueSpecifiedUnits(0, valueInSpecifiedUnits);
-		}
-		public static float GetPXLength(string valueText) {
-			float t_value = 0.0f;
-			uSVGLengthType t_type = uSVGLengthType.SVG_LENGTHTYPE_UNKNOWN;
-			uSVGLengthConvertor.f_ExtractType(valueText, ref t_value, ref t_type);
-			return uSVGLengthConvertor.f_ConvertToPX(t_value, t_type);
-		}
+  /***********************************************************************************/
+  public float value {
+    get { return uSVGLengthConvertor.f_ConvertToPX(m_valueInSpecifiedUnits, m_unitType); }
+  }
+  public uSVGLengthType unitType {
+    get{ return m_unitType;}
+  }
+  /***********************************************************************************/
+  public uSVGLength(ushort unitType, float valueInSpecifiedUnits) {
+    m_unitType = (uSVGLengthType)unitType;
+    m_valueInSpecifiedUnits = valueInSpecifiedUnits;
+  }
+  public uSVGLength(float valueInSpecifiedUnits) {
+    m_unitType = (uSVGLengthType)0;
+    m_valueInSpecifiedUnits = valueInSpecifiedUnits;
+  }
+  public uSVGLength(string valueText) {
+    float t_value = 0.0f;
+    uSVGLengthType t_type = uSVGLengthType.SVG_LENGTHTYPE_UNKNOWN;
+    uSVGLengthConvertor.f_ExtractType(valueText, ref t_value, ref t_type);
+    m_unitType = t_type;      
+    m_valueInSpecifiedUnits = t_value;
+  }
+  /***********************************************************************************/
+  public void NewValueSpecifiedUnits(ushort unitType, float valueInSpecifiedUnits) {
+    m_unitType = (uSVGLengthType)unitType;
+    m_valueInSpecifiedUnits = valueInSpecifiedUnits;
+  }
+  public void NewValueSpecifiedUnits(float valueInSpecifiedUnits) {
+    NewValueSpecifiedUnits(0, valueInSpecifiedUnits);
+  }
+  public static float GetPXLength(string valueText) {
+    float t_value = 0.0f;
+    uSVGLengthType t_type = uSVGLengthType.SVG_LENGTHTYPE_UNKNOWN;
+    uSVGLengthConvertor.f_ExtractType(valueText, ref t_value, ref t_type);
+    return uSVGLengthConvertor.f_ConvertToPX(t_value, t_type);
+  }
 
-		/***********************************************************************************/
-		private float f_GetValue(uSVGLengthType unitType) {
-
-			float temp = uSVGLengthConvertor.f_ConvertToPX(value, m_unitType);
-			switch (unitType) {
-				case uSVGLengthType.SVG_LENGTHTYPE_IN :
-					return temp / 90.0f;
-				case uSVGLengthType.SVG_LENGTHTYPE_CM :
-					return temp / 35.43307f;
-				case uSVGLengthType.SVG_LENGTHTYPE_MM :
-					return temp / 3.543307f;
-				case uSVGLengthType.SVG_LENGTHTYPE_PT :
-					return temp / 1.25f;
-				case uSVGLengthType.SVG_LENGTHTYPE_PC :
-					return temp / 15.0f;
-				default:
-					return temp;
-			}
-		}
+  /***********************************************************************************/
+  private float f_GetValue(uSVGLengthType unitType) {
+    float temp = uSVGLengthConvertor.f_ConvertToPX(value, m_unitType);
+    switch (unitType) {
+      case uSVGLengthType.SVG_LENGTHTYPE_IN:
+        return temp / 90.0f;
+      case uSVGLengthType.SVG_LENGTHTYPE_CM:
+        return temp / 35.43307f;
+      case uSVGLengthType.SVG_LENGTHTYPE_MM:
+        return temp / 3.543307f;
+      case uSVGLengthType.SVG_LENGTHTYPE_PT:
+        return temp / 1.25f;
+      case uSVGLengthType.SVG_LENGTHTYPE_PC:
+        return temp / 15.0f;
+      default:
+        return temp;
+    }
+  }
 }
