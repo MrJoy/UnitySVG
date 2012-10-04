8e2500928dcf713b6d06e5adf6bcfcd513bb5493
Jon Frisby <jfrisby@mrjoy.com>
2012-10-03 19:04:54 -0700

    Formatting cleanup.

--- Assets/Hao_MrJoy/SVG/Resources/Implementation/SVG/DOM/Coordinate_Transform_Units/SVGMatrix.cs
+++ Assets/Hao_MrJoy/SVG/Resources/Implementation/SVG/DOM/Coordinate_Transform_Units/SVGMatrix.cs
@@ -5,8 +5,7 @@ using System;
 public class SVGMatrix {
     protected float _a, _b, _c, _d, _e, _f;
     const double radPerDegree = 2.0 * 3.1415926535 / 360.0;
-    public SVGMatrix() : this(1, 0, 0, 1, 0, 0)
-    {}
+    public SVGMatrix() : this(1, 0, 0, 1, 0, 0) {}
     public SVGMatrix(float a, float b, float c, float d, float e, float f) {
       this._a = a;
       this._b = b;
@@ -49,51 +48,53 @@ public class SVGMatrix {
       sd = secondMatrix.d;
       se = secondMatrix.e;
       sf = secondMatrix.f;
-      return new SVGMatrix(  a*sa + c*sb,    b*sa + d*sb,
-                  a*sc + c*sd,    b*sc + d*sd,
-                  a*se + c*sf + e, b*se + d*sf + f);
+      return new SVGMatrix(a*sa + c*sb,     b*sa + d*sb,
+                           a*sc + c*sd,     b*sc + d*sd,
+                           a*se + c*sf + e, b*se + d*sf + f);
     }
     public SVGMatrix Inverse() {
       double det = a*d - c*b;
       if(det == 0.0) {
         throw new SVGException(SVGExceptionType.MatrixNotInvertable);
       }
-      return new SVGMatrix( (float)(d/det),       (float)(-b/det),
-                 (float)(-c/det),       (float)(a/det),
-                 (float)((c*f - e*d)/ det), (float)((e*b - a*f)/ det));
+      return new SVGMatrix((float)(d/det),            (float)(-b/det),
+                           (float)(-c/det),           (float)(a/det),
+                           (float)((c*f - e*d)/ det), (float)((e*b - a*f)/ det));
     }
     public SVGMatrix Scale(float scaleFactor) {
-      return new SVGMatrix(  a * scaleFactor,   b * scaleFactor,
-                  c * scaleFactor,   d * scaleFactor,
-                  e,          f);
+      return new SVGMatrix(a * scaleFactor, b * scaleFactor,
+                           c * scaleFactor, d * scaleFactor,
+                           e,               f);
     }
     public SVGMatrix ScaleNonUniform(float scaleFactorX, float scaleFactorY) {
-      return new SVGMatrix(  a*scaleFactorX,  b*scaleFactorX,
-                  c*scaleFactorY, d*scaleFactorY,
-                  e,        f);
+      return new SVGMatrix(a*scaleFactorX, b*scaleFactorX,
+                           c*scaleFactorY, d*scaleFactorY,
+                           e,              f);
     }
     public SVGMatrix Rotate(float angle) {
       double ca = (float)Math.Cos((float)(angle * radPerDegree));
       double sa = (float)Math.Sin((float)(angle * radPerDegree));
 
-      return new SVGMatrix( (float)(a*ca + c*sa), (float)(b*ca + d*sa),
-                 (float)(c*ca - a*sa), (float)(d*ca - b*sa),
-                  e,            f);
+      return new SVGMatrix((float)(a*ca + c*sa), (float)(b*ca + d*sa),
+                           (float)(c*ca - a*sa), (float)(d*ca - b*sa),
+                           e,                    f);
     }
     public SVGMatrix Translate(float x, float y) {
-      return new SVGMatrix( a, b, c, d, a*x + c*y + e, b*x + d*y +f);
+      return new SVGMatrix(a,             b,
+                           c,             d,
+                           a*x + c*y + e, b*x + d*y + f);
     }
     public SVGMatrix SkewX(float angle) {
       double ta = Math.Tan((float)(angle*radPerDegree));
-      return new SVGMatrix(  a,          b,
-                 (float)(c + a*ta), (float)(d + b*ta),
-                  e,          f);
+      return new SVGMatrix(a,                 b,
+                           (float)(c + a*ta), (float)(d + b*ta),
+                           e,                 f);
     }
     public SVGMatrix SkewY(float angle) {
       double ta = Math.Tan((float)(angle*radPerDegree));
-      return new SVGMatrix( (float)(a + c*ta), (float)(b + d*ta),
-                  c,          d,
-                  e,          f);
+      return new SVGMatrix((float)(a + c*ta), (float)(b + d*ta),
+                           c,                 d,
+                           e,                 f);
     }
 
   public Vector2 Transform(Vector2 point) {
