aaa3161b255e35be250b5449caf022122378fdf7
Jon Frisby <jfrisby@mrjoy.com>
2016-01-02 17:41:48 -0800

    Another couple formatting situations I missed.

--- Assets/Hao_MrJoy/SVG/Resources/Implement.cs
+++ Assets/Hao_MrJoy/SVG/Resources/Implement.cs
@@ -1,53 +1,45 @@
 using UnityEngine;
 
-public class Implement
-{
-	private TextAsset _SVGFile;
-	private Texture2D _texture;
-	private readonly SVGGraphics _graphics;
-	private SVGDocument _svgDocument;
+public class Implement {
+  private TextAsset _SVGFile;
+  private Texture2D _texture;
+  private readonly SVGGraphics _graphics;
+  private SVGDocument _svgDocument;
 
-	public Implement(TextAsset svgFile, ISVGDevice device)
-	{
-		_SVGFile = svgFile;
-		_graphics = new SVGGraphics(device);
-	}
+  public Implement(TextAsset svgFile, ISVGDevice device) {
+    _SVGFile = svgFile;
+    _graphics = new SVGGraphics(device);
+  }
 
-	private void CreateEmptySVGDocument()
-	{
-		_svgDocument = new SVGDocument(_SVGFile.text, _graphics);
-	}
+  private void CreateEmptySVGDocument() {
+    _svgDocument = new SVGDocument(_SVGFile.text, _graphics);
+  }
 
-	public void StartProcess()
-	{
-		Profiler.BeginSample("SVG.Implement.StartProcess[CreateEmptySVGDocument]");
-		CreateEmptySVGDocument();
-		Profiler.EndSample();
-		Profiler.BeginSample("SVG.Implement.StartProcess[GetRootElement]");
-		SVGSVGElement _rootSVGElement = _svgDocument.RootElement;
-		Profiler.EndSample();
-		Profiler.BeginSample("SVG.Implement.StartProcess[ClearCanvas]");
-		_graphics.SetColor(Color.white);
-		Profiler.EndSample();
-		Profiler.BeginSample("SVG.Implement.StartProcess[RenderSVGElement]");
-		_rootSVGElement.Render();
-		Profiler.EndSample();
-		Profiler.BeginSample("SVG.Implement.StartProcess[RenderGraphics]");
-		_texture = _graphics.Render();
-		Profiler.EndSample();
-	}
+  public void StartProcess() {
+    Profiler.BeginSample("SVG.Implement.StartProcess[CreateEmptySVGDocument]");
+    CreateEmptySVGDocument();
+    Profiler.EndSample();
+    Profiler.BeginSample("SVG.Implement.StartProcess[GetRootElement]");
+    SVGSVGElement _rootSVGElement = _svgDocument.RootElement;
+    Profiler.EndSample();
+    Profiler.BeginSample("SVG.Implement.StartProcess[ClearCanvas]");
+    _graphics.SetColor(Color.white);
+    Profiler.EndSample();
+    Profiler.BeginSample("SVG.Implement.StartProcess[RenderSVGElement]");
+    _rootSVGElement.Render();
+    Profiler.EndSample();
+    Profiler.BeginSample("SVG.Implement.StartProcess[RenderGraphics]");
+    _texture = _graphics.Render();
+    Profiler.EndSample();
+  }
 
-	public void NewSVGFile(TextAsset svgFile)
-	{
-		_SVGFile = svgFile;
-	}
+  public void NewSVGFile(TextAsset svgFile) {
+    _SVGFile = svgFile;
+  }
 
-	public Texture2D GetTexture()
-	{
-		if (_texture == null)
-		{
-			return new Texture2D(0, 0, TextureFormat.ARGB32, false);
-		}
-		return _texture;
-	}
+  public Texture2D GetTexture() {
+    if(_texture == null)
+      return new Texture2D(0, 0, TextureFormat.ARGB32, false);
+    return _texture;
+  }
 }
--- Assets/Hao_MrJoy/SVG/Resources/Interface/RenderingEngine/ISVGPathDraw.cs
+++ Assets/Hao_MrJoy/SVG/Resources/Interface/RenderingEngine/ISVGPathDraw.cs
@@ -1,28 +1,27 @@
 using UnityEngine;
 
-public interface ISVGPathDraw 
-{
-	void MoveTo(Vector2 p);
+public interface ISVGPathDraw {
+  void MoveTo(Vector2 p);
 
-	void CircleTo(Vector2 p, float r);
+  void CircleTo(Vector2 p, float r);
 
-	void EllipseTo(Vector2 p, float r1, float r2, float angle);
+  void EllipseTo(Vector2 p, float r1, float r2, float angle);
 
-	void ArcTo(float r1, float r2, float angle, bool largeArcFlag, bool sweepFlag, Vector2 p);
+  void ArcTo(float r1, float r2, float angle, bool largeArcFlag, bool sweepFlag, Vector2 p);
 
-	void CubicCurveTo(Vector2 p1, Vector2 p2, Vector2 p);
+  void CubicCurveTo(Vector2 p1, Vector2 p2, Vector2 p);
 
-	void QuadraticCurveTo(Vector2 p1, Vector2 p);
+  void QuadraticCurveTo(Vector2 p1, Vector2 p);
 
-	void LineTo(Vector2 p);
+  void LineTo(Vector2 p);
 
-	void Rect(Vector2 p1, Vector2 p2, Vector2 p3, Vector2 p4);
+  void Rect(Vector2 p1, Vector2 p2, Vector2 p3, Vector2 p4);
 
-	void RoundedRect(Vector2 p1, Vector2 p2, Vector2 p3, Vector2 p4, Vector2 p5, Vector2 p6, Vector2 p7, Vector2 p8, float r1, float r2, float angle);
+  void RoundedRect(Vector2 p1, Vector2 p2, Vector2 p3, Vector2 p4, Vector2 p5, Vector2 p6, Vector2 p7, Vector2 p8, float r1, float r2, float angle);
 
-	void Circle(Vector2 p, float r);
+  void Circle(Vector2 p, float r);
 
-	void Ellipse(Vector2 p, float rx, float ry, float angle);
+  void Ellipse(Vector2 p, float rx, float ry, float angle);
 
-	void Polygon(Vector2[] points);
+  void Polygon(Vector2[] points);
 }
