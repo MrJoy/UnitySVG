fe4a99b3784c0a883c3f14710264965c0cb39026
Jon Frisby <jfrisby@mrjoy.com>
2016-01-02 17:31:58 -0800

    Yep, I missed a spot.  Again.

--- Assets/Hao_MrJoy/Use/Invoke.cs
+++ Assets/Hao_MrJoy/Use/Invoke.cs
@@ -3,47 +3,44 @@ using System.Diagnostics;
 using UnityEngine;
 
 [RequireComponent(typeof(Renderer))]
-public class Invoke : MonoBehaviour 
-{
-	public TextAsset SVGFile = null;
-	public bool useFastButBloatedRenderer = false;
+public class Invoke : MonoBehaviour {
+  public TextAsset SVGFile = null;
+  public bool useFastButBloatedRenderer = false;
 
-	private void Start ()
-	{
+  private void Start() {
+    //yield return new WaitForSeconds(0.1f);
+    if(SVGFile != null) {
+      Stopwatch w = new Stopwatch();
 
-		//yield return new WaitForSeconds(0.1f);
-		if (SVGFile != null) {
-			Stopwatch w = new Stopwatch();
+      w.Reset();
+      w.Start();
+      ISVGDevice device;
+      if(useFastButBloatedRenderer)
+        device = new SVGDeviceFast();
+      else
+        device = new SVGDeviceSmall();
+      var implement = new Implement(SVGFile, device);
+      w.Stop();
+      long c = w.ElapsedMilliseconds;
 
-			w.Reset();
-			w.Start();
-			ISVGDevice device;
-			if(useFastButBloatedRenderer)
-				device = new SVGDeviceFast();
-			else
-				device = new SVGDeviceSmall();
-			var implement = new Implement(SVGFile, device);
-			w.Stop();
-			long c = w.ElapsedMilliseconds;
+      w.Reset();
+      w.Start();
+      implement.StartProcess();
+      w.Stop();
+      long p = w.ElapsedMilliseconds;
 
-			w.Reset();
-			w.Start();
-			implement.StartProcess();
-			w.Stop();
-			long p = w.ElapsedMilliseconds;
+      w.Reset();
+      w.Start();
+      var myRenderer = GetComponent<Renderer>();
+      myRenderer.material.mainTexture = implement.GetTexture();
+      w.Stop();
+      long r = w.ElapsedMilliseconds;
+      UnityEngine.Debug.LogFormat("Construction: {0} ms, Processing: {1} ms, Rendering: {2} ms", c, p, r);
 
-			w.Reset();
-			w.Start();
-			var myRenderer = GetComponent<Renderer>();
-			myRenderer.material.mainTexture = implement.GetTexture();
-			w.Stop();
-			long r = w.ElapsedMilliseconds;
-			UnityEngine.Debug.LogFormat("Construction: {0} ms, Processing: {1} ms, Rendering: {2} ms", c, p, r);
-
-			Vector2 ts = myRenderer.material.mainTextureScale;
-			ts.x *= -1;
-			myRenderer.material.mainTextureScale = ts;
-			myRenderer.material.mainTexture.filterMode = FilterMode.Trilinear;
-		}
-	}
+      Vector2 ts = myRenderer.material.mainTextureScale;
+      ts.x *= -1;
+      myRenderer.material.mainTextureScale = ts;
+      myRenderer.material.mainTexture.filterMode = FilterMode.Trilinear;
+    }
+  }
 }
