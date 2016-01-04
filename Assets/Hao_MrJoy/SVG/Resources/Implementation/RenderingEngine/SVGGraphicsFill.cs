3b1805e9a1ada41d4fcc999ba65f7220909b084e
Jon Frisby <jfrisby@mrjoy.com>
2016-01-03 17:37:38 -0800

    Tidy up.

--- Assets/Hao_MrJoy/SVG/Resources/Implementation/RenderingEngine/SVGGraphicsFill.cs
+++ Assets/Hao_MrJoy/SVG/Resources/Implementation/RenderingEngine/SVGGraphicsFill.cs
@@ -91,7 +91,7 @@ public class SVGGraphicsFill : ISVGPathDraw {
   }
 
   private void Fill(int x, int y) {
-//    Debug.LogFormat("Fill called: w:{0}, h:{1}, subW:{2}, subH:{3}, inZoneL:{4}, inZoneT:{5}, x:{6}, y:{7}", _width, _height, _subW, _subH, _inZoneL, _inZoneT, x, y);
+    // Debug.LogFormat("Fill called: w:{0}, h:{1}, subW:{2}, subH:{3}, inZoneL:{4}, inZoneT:{5}, x:{6}, y:{7}", _width, _height, _subW, _subH, _inZoneL, _inZoneT, x, y);
     Profiler.BeginSample("SVGGraphicsFill.Fill");
     if(!isInZone(x, y) || _flag[x, y] != 0)
       return;
@@ -115,7 +115,7 @@ public class SVGGraphicsFill : ISVGPathDraw {
         }
       }
     }
-//    Debug.LogFormat("NbIter:{0}", nbIterations);
+    // Debug.LogFormat("NbIter:{0}", nbIterations);
     Profiler.EndSample();
   }
 
@@ -148,7 +148,7 @@ public class SVGGraphicsFill : ISVGPathDraw {
     if(_boundBottomRight.y >= _height)
       _boundBottomRight.y = _height - 1f;
 
-    _subW = Math.Abs((int)_boundTopLeft.x - (int)_boundBottomRight.x) + 3; // was + 1 + (2*1), but no idea why
+    _subW = Math.Abs((int)_boundTopLeft.x - (int)_boundBottomRight.x) + 3;
     _subH = Math.Abs((int)_boundTopLeft.y - (int)_boundBottomRight.y) + 3;
 
     _inZoneL = (int)_boundTopLeft.x - 1;
@@ -164,7 +164,7 @@ public class SVGGraphicsFill : ISVGPathDraw {
     _subH = (_subH + _inZoneT > _height) ? (_height - _inZoneT) : _subH;
 
     Fill(_inZoneL, _inZoneT);
-    // This seems buggy:
+    // TODO: This seems buggy:
     if((_inZoneL == 0) && (_inZoneT == 0))
       Fill(_subW - 1, _subH - 1);
   }
